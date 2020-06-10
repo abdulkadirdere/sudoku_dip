0b41e299af1a2a41e6c5a537f95102607aa3d90e
Abdulkadir Dere <abduldere@yahoo.com>
2020-06-10 22:30:58 +0200

    cleaned the code by creating functions for the processes

--- src/sudoku.m
+++ src/sudoku.m
@@ -2,63 +2,17 @@ clear all;
 clc;
 
 % read in the RGB sudoku puzzle image
-sudoku_image1 = imread("../data/sudoku14.png");
+sudoku_image = imread("../data/sudoku2.png");
 
-% get the puzzle image in 2D
-grayImage = rgb2gray(sudoku_image1);
-binary_image = imbinarize(grayImage);
-% imshow(binary_image);
+% preprocess of input image to prepare for the image processing techniques
+% to be used
+[image, bounding_box] = preImageProcessing(sudoku_image);
 
-% get complement of binary image so foreground pixels are white i.e. 1
-comp_image = imcomplement(binary_image);
-% imshow(comp_image);
+plotImageBoundingBox(image, bounding_box);
 
-sudoku_frame = bwareafilt(comp_image, 1, 'largest');
-% imshow(sudoku_frame);
+result = OCR(image, bounding_box);
 
-% image cropping
-% find the framing square and crop the image accordingly. Label is done on
-% the frame and not numbers.
-labelled = bwlabel(sudoku_frame);
-[row, column]=find(labelled==1);
-cropped = sudoku_frame(min(row):max(row), min(column):max(column));
-% imshow(cropped);
-
-% get the location of each box in the sudoku image
-stats = regionprops(imcomplement(cropped),'BoundingBox');
-
-% store all the x & y coord of bounding boxes
-bounding_box = cat(1, stats.BoundingBox);
-
-% crop original image with numbers in it. We will work on this image
-image = comp_image(min(row):max(row), min(column):max(column));
-
-
-% plot the bounding boxes on the image
-% imshow(~image)
-% hold on
-% plot(bounding_box(:,1), bounding_box(:,2),'r*')
-% rectangle('Position', [bounding_box(1,1), bounding_box(1,2), bounding_box(1,3), bounding_box(1,4)], 'EdgeColor','r');
-% hold off
-
-
-% get number of boxes
-num_box = size(bounding_box,1);
-result = zeros(9,9);
-
-% kernel for each number from 1 to 9
-number_kernels = ["num1.mat","num2.mat","num3.mat","num4.mat","num5.mat","num6.mat","num7.mat","num8.mat","num9.mat"];
-
-for i = 1:size(number_kernels,2)
-    kernel = load("kernel/"+number_kernels(i)).SE;
-    kernel_value = i;
-    result = hitormiss(image, bounding_box, result, kernel, kernel_value, num_box);
-    disp(result);
-end
-
-% write results to CSV and XLS fiels to be consumed by a sudoku solver
-writematrix(result,'output/sudoku_puzzle.csv');
-writematrix(result,'output/sudoku_puzzle.xls');
+writeResults(result);
 
 % display results in heatmap
 figure,
@@ -66,6 +20,47 @@ heatmap(result);
 
 
 %% functions
+
+function [image, bounding_box, comp_image] = preImageProcessing(sudoku_image)
+    % get the puzzle image in 2D
+    grayImage = rgb2gray(sudoku_image);
+    binary_image = imbinarize(grayImage);
+    % imshow(binary_image);
+
+    % get complement of binary image so foreground pixels are white i.e. 1
+    comp_image = imcomplement(binary_image);
+    % imshow(comp_image);
+
+    sudoku_frame = bwareafilt(comp_image, 1, 'largest');
+%     imshow(sudoku_frame);
+
+    % image cropping
+    % find the framing square and crop the image accordingly. Label is done on
+    % the frame and not numbers.
+    labelled = bwlabel(sudoku_frame);
+    [row, column]=find(labelled==1);
+    cropped = sudoku_frame(min(row):max(row), min(column):max(column));
+    imshow(~cropped);
+      imwrite(~cropped, 'frame.png');
+
+    % get the location of each box in the sudoku image
+    stats = regionprops(imcomplement(cropped),'BoundingBox');
+
+    % store all the x & y coord of bounding boxes
+    bounding_box = cat(1, stats.BoundingBox);
+
+    % crop original image with numbers in it. We will work on this image
+    image = comp_image(min(row):max(row), min(column):max(column));
+end
+
+function plotImageBoundingBox(image, bounding_box)
+    % plot the bounding boxes on the image
+    imshow(~image)
+    hold on
+    plot(bounding_box(:,1), bounding_box(:,2),'r*')
+    rectangle('Position', [bounding_box(1,1), bounding_box(1,2), bounding_box(1,3), bounding_box(1,4)], 'EdgeColor','r');
+    hold off
+end
 
 function result = hitormiss(image, bounding_box, location, kernel, kernel_value, num_box)
 SE = kernel;
@@ -103,4 +98,30 @@ function match = matching(sub)
             end
         end
     end
+end
+
+function kernels = obtainKernels()
+    % kernel for each number from 1 to 9
+    kernels = ["num1.mat","num2.mat","num3.mat","num4.mat","num5.mat","num6.mat","num7.mat","num8.mat","num9.mat"];
+end
+
+function result = OCR(image, bounding_box)
+    % get number of boxes
+    num_box = size(bounding_box,1);
+    result = zeros(9,9);
+    
+    kernels = obtainKernels();
+
+    for i = 1:size(kernels,2)
+        kernel = load("kernel/"+kernels(i)).SE;
+        kernel_value = i;
+        result = hitormiss(image, bounding_box, result, kernel, kernel_value, num_box);
+        disp(result);
+    end
+end
+
+function writeResults(result)
+    % write results to CSV and XLS fiels to be consumed by a sudoku solver
+    writematrix(result,'output/sudoku_puzzle.csv');
+    writematrix(result,'output/sudoku_puzzle.xls');
 end
